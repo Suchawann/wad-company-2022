453dff1e84c7a8f6cc843a7078af8e46f1ddbfc8
unknown <u6135102@au.edu>
2022-03-20 18:55:06 +0700

    added quotation management

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/customers', customerRouter);
 app.use('/quotations', quotationRouter);
 
 // connect to frontend
-app.use(express.static(path.join(__dirname, 'public', 'react-quotation'), { index: false }));
+app.use(express.static(path.join(__dirname, 'public', 'react-quotation'), {index: '_'}));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'react-quotation', 'index.html'));
