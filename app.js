e0df31eb808c900cd74b2950660426ebd3733e65
unknown <u6135102@au.edu>
2022-03-20 17:48:50 +0700

    added quotation management

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/quotations', quotationRouter);
 app.use(express.static(path.join(__dirname, 'public', 'react-quotation')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'react-quotation', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/react-quotation', 'index.html'));
 });
 
 // catch 404 and forward to error handler
