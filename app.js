3aa5c493dd521179748ad065d4d0f2e10f339aa8
unknown <u6135102@au.edu>
2022-03-20 19:10:07 +0700

    added quotation management

--- app.js
+++ app.js
@@ -34,11 +34,12 @@ app.use('/customers', customerRouter);
 app.use('/quotations', quotationRouter);
 
 // connect to frontend
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'react-quotation'));
-});
+app.use(express.static(path.join(__dirname, 'public', 'react-quotation')));
 
-app.use(express.static(path.join(__dirname, 'public', 'react-quotation'), {index: false}));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'react-quotation', 'index.html'));
+  res.redirect('/react-quotation/quotation');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
